fe05921dc2741ceb2716ee003d56cd65f5987011
Sarah Corey <syncorey@gmail.com>
2016-01-24 23:32:25 -0500

    shipped

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
 		@boats = Boat.new
 	end
 	def edit
-		@user=User.find(params[:id])
+		@user = User.find(params[:id])
 	end
 	def update
 		@user = User.find(params[:id])
--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,11 @@
 class Boat < ActiveRecord::Base
+  validates :name, uniqueness: true, on: :create
+  validates :location, inclusion: {in: %w('Halifax', 'Havana', 'Jakarta', 'Los Angeles', 'Split')}
+  # validates presence of array of locales [Jakarta, Havana, Split, Los Angeles, Halifax]
   belongs_to :user
   has_many :jobs, dependent: :destroy
+
+  has_attached_file :avatar, :styles => { :thumb => "60x60>" }, :default_url => "/images/:style/missing.png" 
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  
 end
