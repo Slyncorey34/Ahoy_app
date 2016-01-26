4b68593443782fb2835762ed5b2fef54e9671eab
Sarah Corey <syncorey@gmail.com>
2016-01-25 19:07:29 -0500

    Add new boat and display on User page

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -10,16 +10,19 @@ class BoatsController < ApplicationController
 
   def new
   	@boat = Boat.new
-    @user= current_user
+    @user = current_user
   end
 
   def create
     @boat = current_user.boats.build(boat_params)
-      if @boat.save
-        redirect_to user_path(current_user)
-      else
-        flash[:alert] = "Whoops. Make sure all fields are filled."
-      end
+    if @boat.save
+      puts "?????????????????????????????????"
+      redirect_to user_path(current_user)
+    else
+      p "?????????????????????????????????else render"
+      flash[:alert] = "Whoops. Make sure all fields are filled."
+      render new_boat_path
+    end
   end
 # 	  @boat = current_user.boats.build(boat_params)
 # 	 	if @boat.save
@@ -29,33 +32,33 @@ class BoatsController < ApplicationController
 #   end
 # end
 
-   def edit
-    @boat = Boat.find(params[:id])
-  end
+def edit
+  @boat = Boat.find(params[:id])
+end
 
-  def update
-    @boat = Boat.find(params[:id])
-    if @boat.update_attributes(boat_params)
-      redirect_to boat_path(@boat)
-    else
-      render edit_boat_path(@boat) 
-    end	
-  end
+def update
+  @boat = Boat.find(params[:id])
+  if @boat.update_attributes(boat_params)
+    redirect_to boat_path(@boat)
+  else
+    render edit_boat_path(@boat) 
+  end	
+end
 
-  def destroy
-  	@user = User.find(params[:id])
-  	@boat= Boat.find(params[:id])
-  		if @user == current_user
-  			@boat.destroy
-  			flash[:alert] = "Shipment cancelled"
-  		else
-  			flash[:alert] = "You can't edit or cancel this shipment."
-	  end
-	  	redirect_to user_path(@user)
-	  end
+def destroy
+ @user = User.find(params[:id])
+ @boat= Boat.find(params[:id])
+ if @user == current_user
+   @boat.destroy
+   flash[:alert] = "Shipment cancelled"
+ else
+   flash[:alert] = "You can't edit or cancel this shipment."
+ end
+ redirect_to user_path(@user)
+end
 
-  private
-  	def boat_params
-  		params.require(:boat).permit(:name, :location, :container, :avatar)
-  	end
+private
+def boat_params
+  params.require(:boat).permit(:name, :location, :container, :avatar, :user_id)
+end
 end
